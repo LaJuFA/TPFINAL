c8331c27fe5d6e2106905e6962a19eeaf4a0eef2
Perkin <lauljfa05@gmail.com>
2022-07-13 14:54:54 -0300

    Commit 3

---
+++ TP Final/JS/script.js
@@ -0,0 +1,8 @@
+function NocFunction() {
+  document.getElementById('SS').style.backgroundColor = '#998066';
+  document.getElementById('SS').style.color = '#FFFFFF';
+  document.getElementById('SD').style.backgroundColor = '#7a6752';
+  document.getElementById('SD').style.color = '#FFFFFF';
+  document.getElementById('SR').style.backgroundColor = '#998066';
+  document.getElementById('SR').style.color = '#FFFFFF';
+}
